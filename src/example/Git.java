3e26ce797c09c21667e0e8301ae1984c33bc7204
lq3600 <225020006@qq.com>
2019-02-27 01:49:32 +0800

    1.0

--- src/example/Git.java
+++ src/example/Git.java
@@ -1,9 +1,9 @@
 package example;
 
 /**
- * Created by admin on 2019/2/27.
- * 提交到github 测试
- */
+* Created by admin on 2019/2/27.
+* 提交到github 测试
+*/
 public class Git {
 
     public static void main(String[] argv) {
